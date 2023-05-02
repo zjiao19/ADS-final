75b9658338ff1baa3a316b291c8201779f809b70
zjiao19 <zjiao19@outlook.com>
2023-05-02 02:40:27 -0400

    goal based

--- gameapp.py
+++ gameapp.py
@@ -1,32 +1,11 @@
 #!/usr/bin/env python
-from flask import Flask, request, make_response, redirect, url_for
+from flask import Flask, render_template_string, request, make_response, redirect, url_for
 from flask import render_template
 import json
+import goal_based
 
 app = Flask(__name__, template_folder='.')
 
-game_goals = {
-    'play with friends': ['MMO', 'FPS', 'sports'],
-    'building skills': ['puzzle', 'simulation', 'strategy'],
-    'storytelling': ['RPG', 'adventure', 'visual novel'],
-    'exploration': ['sandbox', 'open-world', 'platformer'],
-    'competition': ['fighting', 'racing', 'sports'],
-    'relaxation': ['casual', 'simulation', 'music'],
-    'creativity': ['sandbox', 'construction', 'artistic'],
-    'immersion': ['RPG', 'adventure', 'horror'],
-    'problem-solving': ['puzzle', 'adventure', 'strategy'],
-    'collection-building': ['RPG', 'card game', 'gacha'],
-    'role-playing': ['RPG', 'simulation', 'strategy'],
-    'community-building': ['MMO', 'simulation', 'city-building'],
-    'skill-building': ['simulation', 'sports', 'puzzle'],
-    'experimentation': ['sandbox', 'simulation', 'strategy'],
-    'strategy': ['RTS', 'tactics', 'simulation'],
-    'mystery-solving': ['adventure', 'puzzle', 'visual novel'],
-    'action': ['FPS', 'fighting', 'platformer'],
-    'science-fiction': ['RPG', 'FPS', 'strategy'],
-    'fantasy': ['RPG', 'strategy', 'visual novel'],
-    'horror': ['survival', 'adventure', 'visual novel']
-}
 popular_games = [
     'Grand Theft Auto V',
     'Minecraft',
@@ -55,7 +34,7 @@ popular_games = [
 @app.route('/index', methods=['GET'])
 def index():
     html = render_template(
-        'index.html', game_goals=game_goals.keys(), popular_games=popular_games)
+        'index.html', game_goals=goal_based.game_goals.keys(), popular_games=popular_games)
     response = make_response(html)
     return response
 
@@ -66,30 +45,33 @@ def create_user():
         d = json.loads(request.data.decode())
         fav_games = d["games"]
         goals = d["goals"]
-        print("fav_games", fav_games)
-        print("goals", goals)
-    return make_response("0")
+        # print("fav_games", fav_games)
+        # print("goals", goals)
+        agent = goal_based.Agent(goals)
+        return make_response(str(agent.id))
 
 
 @app.route('/result/<user_id>', methods=['GET'])
 def result(user_id):
-
-    data = [
-        {
-            "title": "Apple",
-            "img": "https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?201606271147",
-            "description": "Apple is awesome",
-            "reason": "You use iPhone",
-            "rating": 4,
-        },
-        {
-            "title": "Banana",
-            "img": "https://media.istockphoto.com/id/1400057530/photo/bananas-isolated.jpg?b=1&s=170667a&w=0&k=20&c=uiSdjIQkTr7S4gEdW_oB_5zfFYhpfe0LP-CryQl49w4=",
-            "description": "Banana is bwesome",
-            "reason": "You are Asian",
-            "rating": 3,
-        },
-    ]
-    html = render_template('result.html', length=len(data), data=data)
+    if user_id == "None":
+        # img not found
+        return render_template_string('PageNotFound {{ errorCode }}', errorCode='404'), 404
+    if int(user_id) >= len(goal_based.Agent.all_agents):
+        # user not found
+        return redirect(url_for('index'))
+    
+    agent = goal_based.Agent.all_agents[int(user_id)]
+    recommendations = goal_based.get_recommendations(agent)
+    data = []
+    for r in recommendations:
+        data.append({
+            "title": r.game.name,
+            "img": r.game.cover,
+            "description": r.game.description,
+            "reason": ", ".join(r.reasons),
+            "rating": r.game.rating,
+            "url": r.game.url
+        })
+    html = render_template('result.html', data=data)
     response = make_response(html)
     return response
